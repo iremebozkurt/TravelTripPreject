72314692b6ee4b172d8ab8147192c10c498be77c
İrem Bozkurt <iebozkurt@ggmail.com>
2023-12-27 15:49:18 +0300

    Proje dosyası ekle.

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripPreject2.Models.Siniflar;
+
+namespace TravelTripPreject2.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var bloglar = c.Blogs.ToList();
+            return View(bloglar);
+        }
+        public ActionResult BlogDetay(int id)
+        {
+            var blogbul = c.Blogs.Where(x=>x.ID==id).ToList();
+            return View(blogbul);
+        }
+      
+    }
+}
---
+++ Handlers/AdminHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TravelTripPreject2.Models.Siniflar
+{
+    public class AdminHandler
+    {
+        Context c = new Context();
+
+        public List<Blog> ListBlog()
+        {
+            var degerler = c.Blogs.ToList();
+
+            return degerler;
+        }
+
+    }
+}
